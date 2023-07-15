b356e64c846d2e15c8eef92ba8ca19af3b129f0a
Eliaskh77 <20316165+Eliaskh77@users.noreply.github.com>
2023-07-15 14:10:13 +0330

    finish html and add style to main page

---
+++ app.js
@@ -0,0 +1,7 @@
+// todo my todo's as object created with factory or class
+// todo create different modules for different task
+//todo The look of the User Interface is up to you, but it should be able to do the following:
+// view all projects
+// view all todos in each project (probably just the title and duedate… perhaps changing color for different priorities)
+// expand a single todo to see/edit its details
+// delete a todo
